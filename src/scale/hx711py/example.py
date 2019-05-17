ad4c4daef563651d80822d1b2e6d049a23fbdc17
giuliol <giulio.luzzati@edu.unige.it>
2019-05-17 21:49:35 +0100

    implemented scale

--- src/scale/hx711py/example.py
+++ src/scale/hx711py/example.py
@@ -1,24 +1,27 @@
 #! /usr/bin/python2
 
-import time
 import sys
 
-EMULATE_HX711=False
+EMULATE_HX711 = False
 
 if not EMULATE_HX711:
-    import RPi.GPIO as GPIO
-    from hx711 import HX711
+	import RPi.GPIO as GPIO
+	from hx711 import HX711
 else:
-    from emulated_hx711 import HX711
+	from emulated_hx711 import HX711
+
 
 def cleanAndExit():
-    print "Cleaning..."
+	print
+	"Cleaning..."
 
-    if not EMULATE_HX711:
-        GPIO.cleanup()
-        
-    print "Bye!"
-    sys.exit()
+	if not EMULATE_HX711:
+		GPIO.cleanup()
+
+	print
+	"Bye!"
+	sys.exit()
+
 
 hx = HX711(23, 24)
 
@@ -36,42 +39,44 @@ hx.set_reading_format("MSB", "MSB")
 # In this case, 92 is 1 gram because, with 1 as a reference unit I got numbers near 0 without any weight
 # and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
 # If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
-#hx.set_reference_unit(113)
+# hx.set_reference_unit(113)
 hx.set_reference_unit(730)
 
-hx.reset()
+hx.reset_and_tare()
 
 hx.tare()
 
-print "Tare done! Add weight now..."
+print
+"Tare done! Add weight now..."
 
 # to use both channels, you'll need to tare them both
-#hx.tare_A()
-#hx.tare_B()
+# hx.tare_A()
+# hx.tare_B()
 
 while True:
-    try:
-        # These three lines are usefull to debug wether to use MSB or LSB in the reading formats
-        # for the first parameter of "hx.set_reading_format("LSB", "MSB")".
-        # Comment the two lines "val = hx.get_weight(5)" and "print val" and uncomment these three lines to see what it prints.
-        
-        # np_arr8_string = hx.get_np_arr8_string()
-        # binary_string = hx.get_binary_string()
-        # print binary_string + " " + np_arr8_string
-        
-        # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
-        val = hx.get_weight(1)
-        print val
+	try:
+		# These three lines are usefull to debug wether to use MSB or LSB in the reading formats
+		# for the first parameter of "hx.set_reading_format("LSB", "MSB")".
+		# Comment the two lines "val = hx.get_weight(5)" and "print val" and uncomment these three lines to see what it prints.
 
-        # To get weight from both channels (if you have load cells hooked up 
-        # to both channel A and B), do something like this
-        #val_A = hx.get_weight_A(5)
-        #val_B = hx.get_weight_B(5)
-        #print "A: %s  B: %s" % ( val_A, val_B )
+		# np_arr8_string = hx.get_np_arr8_string()
+		# binary_string = hx.get_binary_string()
+		# print binary_string + " " + np_arr8_string
 
-       # hx.power_down()
-       # hx.power_up()
-       # time.sleep(0.01)
+		# Prints the weight. Comment if you're debbuging the MSB and LSB issue.
+		val = hx.get_weight(1)
+		print
+		val
 
-    except (KeyboardInterrupt, SystemExit):
-        cleanAndExit()
+	# To get weight from both channels (if you have load cells hooked up
+	# to both channel A and B), do something like this
+	# val_A = hx.get_weight_A(5)
+	# val_B = hx.get_weight_B(5)
+	# print "A: %s  B: %s" % ( val_A, val_B )
+
+	# hx.power_down()
+	# hx.power_up()
+	# time.sleep(0.01)
+
+	except (KeyboardInterrupt, SystemExit):
+		cleanAndExit()
--- src/scale/scale.py
+++ src/scale/scale.py
@@ -1,10 +1,29 @@
+import RPi.GPIO as GPIO
+
+from scale.hx711.hx711 import HX711
+
+DOUT = 23
+SCK = 24
+UNIT_SCALE = 730
+
+
 class Scale:
 	def __init__(self):
+		self.hx = HX711(DOUT, SCK)
+		self.hx.set_reading_format("MSB", "MSB")
+		self.hx.set_reference_unit(UNIT_SCALE)
+		self.reset_and_tare()
 		self.init = True
-		pass
+
+	def reset_and_tare(self):
+		self.hx.reset()
+		self.hx.tare()
+
+	def __del__(self):
+		GPIO.cleanup()
 
 	def get_weight(self):
-		pass
+		return self.hx.get_weight()
 
 
 class ScaleEmulator(Scale):
