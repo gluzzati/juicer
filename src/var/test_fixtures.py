b0634233481441e634b85af13ed2874ea81f9f1a
Giulio Luzzati <gluzzati@camtouch3d.com>
2019-05-21 10:12:05 +0100

    deleted var folder

--- src/var/test_fixtures.py
+++
@@ -1,7 +0,0 @@
-import os
-here = os.path.dirname(os.path.realpath(__file__))
-
-rfid_json = open(here + "/rfid.json", "r").read()
-weight_json = open(here + "/weight.json", "r").read()
-valid_json = open(here + "/valid.json", "r").read()
-invalid_json = "whoa there"
